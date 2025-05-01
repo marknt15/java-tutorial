109b96c7bcd208f50720a33d71baceda75e32761
Mark Edward Tan <marknt15@gmail.com>
2025-05-02 00:52:38 +0800

    Create reading-input.java

---
+++ reading-input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    Scanner scanner = new Scanner(System.in);
+
+    byte age = scanner.nextByte();
+    int someNumber = scanner.nextInt();
+
+    // .next() <--- for string
+    // .nextLine() <--- reads full line
+    
+}
