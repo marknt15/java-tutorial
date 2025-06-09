245521603cea2164c378a998932709790fb0e8f2
Mark Edward Tan <marknt15@gmail.com>
2025-06-09 13:53:12 +0800

    Update springboot-faqs.java

--- springboot-faqs.java
+++ springboot-faqs.java
@@ -1,1 +1,27 @@
-// How to add a Bean using Annotation
+// How to add a Bean using Annotations
+// Other Annotations: @Component @Service @Repository @Controller
+
+@SpringbootApplication
+public class StoreApplication {
+  public static void main(String[] args) {
+    ApplicationContext context = SpringApplication.run(StoreApplication.);
+    var orderService = context.getBean(OrderService.class);
+    orderService.placeOrder();
+  }
+}
+
+// OrderService
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderService {
+  private PaymentService paymentService;
+
+  public OrderService(PaymentService paymentService) {
+    this.paymentService = paymentService;
+  }
+
+  public void placeOrder() {
+    paymentService.processPayment(10);
+  }
+}
